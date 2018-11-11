8a8984e559fcc6c5772b9440ec33d71d47c4aca6
vagrant <vagrant@homestead>
2018-11-11 18:23:48 +0000

    Finish basic page layout

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+	<div class="container">
+		<p class="pull-left">
+			由<a href="https://github.com/Rotor1996" target="_blank">Rotor</a>设计和编码<span style="color: #e27575;font-size: 14px;">❤</span>
+		</p>
+		<p class="pull-right"><a href="https://laravel-china.org/">感谢Laravel中文论坛</a></p>
+	</div>
+</footer>
