51f753b7c89f45ef2009d860021ad261dc68060c
vagrant <vagrant@homestead>
2018-11-22 07:59:36 +0000

    Topic detail page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
 	<title>@yield('title','LaraBBS') - Laravel BBS</title>
+	<meta name="description" content="@yield('description','LaraBBS')"/>
 	<!-- Styles -->
 	<link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
 	@yield('styles')
