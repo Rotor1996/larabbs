24145d156c5a89926b2e1f38489cdac45975edb4
vagrant <vagrant@homestead>
2018-11-23 05:56:10 +0000

    Fix page 404 caused by case in Topics.php

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -7,7 +7,7 @@ class Topic extends Model
     protected $fillable = ['title', 'body', 'category_id','excerpt', 'slug'];
     public function category()
     {
-    	return $this->belongsTo(category::class);
+    	return $this->belongsTo(Category::class);
     }
     public function user()
     {
--- app/Modls/Category.php
+++ app/Modls/Category.php
@@ -1,10 +1,10 @@
 <?php
-
-namespace App\Modls;
+namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
 class Category extends Model
 {
-    //
+	protected $fillable=[
+		'name','description',
+	];
 }
