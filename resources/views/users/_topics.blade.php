a2ac05a7101fbe5b2e7e8ba0f3235742263479e9
vagrant <vagrant@homestead>
2018-11-24 09:12:24 +0000

    List user replies

--- resources/views/users/_topics.blade.php
+++ resources/views/users/_topics.blade.php
@@ -2,7 +2,7 @@
 <ul class="list-group">
 	@foreach($topics as $topic)
 	<li class="list-group-item">
-		<a href="{{ $toupic->link() }}">
+		<a href="{{ $topic->link() }}">
 			{{ $topic->title }}
 		</a>
 		<span class="meta pull-right">
