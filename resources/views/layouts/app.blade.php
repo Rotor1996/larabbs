0843b8d63604d02c89957be38dfec5b7c34d50a5
vagrant <vagrant@homestead>
2018-11-25 18:02:02 +0000

    Integrated administrator

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,14 +21,11 @@
 		</div>
 		@include('layouts._footer')
 	</div>
-	<!-- Scripts -->
-	<script type="text/javascript" src="{{ asset('js/app.js') }}"></script>
-	@yield('scripts')
-	@if (app()->isLocal())
-        @include('sudosu::user-selector')
-    @endif
-
+	    @if(app()->isLocal())
+    	@include('sudosu::user-selector')
+    	@endif
     <!-- Scripts -->
+
     <script src="{{ asset('js/app.js') }}"></script>
     @yield('scripts')
 </body>
